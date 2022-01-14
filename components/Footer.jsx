26d8863b947554737c8da3fb5b5ec4e2c236868e
Samet Öztürk <0zturksamet4901@gmail.com>
2022-01-14 19:45:15 +0100

    secondDay

---
+++ components/Footer.jsx
@@ -0,0 +1,16 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+const Footer = () => {
+  <Box
+    textAlign="cnter"
+    p="5"
+    color="gray.500"
+    borderTop="1px"
+    borderColor="gray100"
+  >
+    2022 RealTor,Inc
+  </Box>;
+};
+
+export default Footer;
